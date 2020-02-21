66d4909cc4b6f5c29ca2809c8d098115d606de13
Vatazhyshyn <vatazishin@gmail.com>
2020-02-21 10:03:26 +0200

    final version

--- js/script.js
+++ js/script.js
@@ -106,19 +106,15 @@ window.addEventListener('DOMContentLoaded', function() {
   //order
   const buttonsOrder = document.querySelectorAll('.button_small');
   const modalOrder = document.getElementById('order');
-  const modalOrderDescription = document.querySelector(
-    '#order .modal__description'
-  );
   const catalogTitles = document.querySelectorAll('.catalog-item__title');
-  let productValue = document.querySelector('[name=product]').value;
+  const productName = document.getElementById('product');
 
   buttonsOrder.forEach((elem, index) => {
     elem.addEventListener('click', function() {
       for (let i = 0; i < catalogTitles.length; i++) {
         if (index == i) {
-          modalOrderDescription.innerHTML = catalogTitles[i].textContent;
-          productValue = catalogTitles[i].textContent;
-          console.log(productValue);
+          productName.value = catalogTitles[i].textContent;
+          break;
         }
       }
       overlay.style.display = 'block';
@@ -127,22 +123,102 @@ window.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-  //email
-  $('form').submit(function(e) {
-    e.preventDefault();
-    $.ajax({
-      type: 'POST',
-      url: 'mailer/smart.php',
-      data: $(this).serialize()
-    }).done(function() {
-      $(this)
-        .find('input')
-        .val('');
-      $('#consultation, #order').fadeOut();
-      $('.overlay, #thanks').fadeIn('slow');
+  // SLIDER
+  let slideIndex = 1;
+  const slides = document.querySelectorAll('.slider-item');
+  const prev = document.querySelector('.prev');
+  const next = document.querySelector('.next');
+  const dotsWrap = document.querySelector('.slider-dots');
+  const dots = document.querySelectorAll('.dot');
 
-      $('form').trigger('reset');
-    });
-    return false;
+  showSlides(slideIndex);
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    slides.forEach(item => (item.style.display = 'none'));
+    dots.forEach(item => item.classList.remove('dot-active'));
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('dot-active');
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  function currentSlide(n) {
+    showSlides((slideIndex = n));
+  }
+
+  prev.addEventListener('click', () => {
+    plusSlides(-1);
   });
+
+  next.addEventListener('click', () => {
+    plusSlides(1);
+  });
+
+  dotsWrap.addEventListener('click', event => {
+    for (let i = 0; i < dots.length + 1; i++) {
+      if (
+        event.target.classList.contains('dot') &&
+        event.target == dots[i - 1]
+      ) {
+        currentSlide(i);
+      }
+    }
+  });
+
+  const swichSlide = setInterval(() => plusSlides(1), 8000);
+
+  //page up icon
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  //animate feedback
+  new WOW().init();
+
+  //validation
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: 'required',
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Введіть своє ім'я",
+          minlength: jQuery.validator.format('Введіть принаймні {0} символи!')
+        },
+        phone: 'Введіть номер телефону',
+        email: {
+          required: 'Введіть свою електронну адресу',
+          email: 'Неправильна електронна адреса'
+        }
+      }
+    });
+  }
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+
+  //phone_mask
+  $('input[name=phone]').mask(' +38 (999) 999-99-99');
 });
